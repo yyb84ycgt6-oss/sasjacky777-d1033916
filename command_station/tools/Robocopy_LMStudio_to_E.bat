03b0c61b68a4201bfcfa7a5d7771d1c0dea13409
yyb84ycgt6-oss <yyb84ycgt6@privaterelay.appleid.com>
2026-08-28 01:09:23 -0700

    Update Robocopy_LMStudio_to_E.bat with full content

--- command_station/tools/Robocopy_LMStudio_to_E.bat
+++ command_station/tools/Robocopy_LMStudio_to_E.bat
@@ -1,4 +1,7 @@
 @echo off
+setlocal enabledelayedexpansion
+
 set "SOURCE_DIR=C:/Users/Eru/AI_ Workspace/models/LM_Studio/Models"
 set "DEST_DIR=E:/AI_Permanent/Models"
-robocopy "%SOURCE_DIR%" "%DEST_DIR%" /MIR /R:1 /W:1 /Z /E
+
+robocopy "%SOURCE_DIR%" "%DEST_DIR%" /MIR /R:1 /W:1 /Z /E /COPY:DAT /LOG:"C:/Users/Eru/AI_ Workspace/logs/robocopy_mirror_%date%.log"
